a26f58a20c38d8349208d5ff9c5312977739f81f
Giovanni <giovanni.benussi@usach.cl>
2014-01-04 00:02:58 -0300

    Función que añade un string al mensaje
    Puede especificar si se añade al inicio o al final.

--- enlace.cc
+++ enlace.cc
@@ -8,15 +8,34 @@
 #include <string.h>
 #include <omnetpp.h>
 
+#define ANADIR_AL_INICIO 0
+#define ANADIR_AL_FINAL  1
+
 class enlace : public cSimpleModule
 {
   protected:
     	virtual void processMsgFromHigherLayer(cMessage *dato);
     	virtual void processMsgFromLowerLayer(cMessage *packet);
     	virtual void handleMessage(cMessage *msg);
+    	virtual cMessage * AnadirMensajeACMessage(cMessage * msg, char * mensaje, int posicion);
 };
 
 Define_Module( enlace );
+
+cMessage * enlace::AnadirMensajeACMessage(cMessage * msg, char * mensaje, int posicion){
+    const char * a_mensaje_anterior = msg->getFullName();
+    int largo_total = 1 + strlen(a_mensaje_anterior) + strlen(mensaje);
+    char * mensaje_nuevo = (char*) malloc(sizeof(char)*largo_total);
+    if(posicion == ANADIR_AL_FINAL){
+        strcpy(mensaje_nuevo, a_mensaje_anterior);
+        strcat(mensaje_nuevo, mensaje);
+    }else{
+        strcpy(mensaje_nuevo, mensaje);
+        strcat(mensaje_nuevo, a_mensaje_anterior);
+    }
+    cMessage * nuevo = new cMessage(mensaje_nuevo);
+    return nuevo;
+}
 
 void enlace::handleMessage(cMessage *msg)
 {
@@ -27,7 +46,9 @@ void enlace::handleMessage(cMessage *msg)
 //lo que se hace cuando llega una palabra de informaciÃ²n desde un modulo superior
 void enlace::processMsgFromHigherLayer(cMessage *dato)
 {
-    	send(dato,"hacia_fisico");
+    // Ejemplo de como añadir un dato al final del mensaje
+    dato = AnadirMensajeACMessage(dato, "0101", ANADIR_AL_FINAL);
+    send(dato,"hacia_fisico");
 }
 //lo que se hace cuando llega una palabra de codigo desde otro host
 void enlace::processMsgFromLowerLayer(cMessage *packet)
